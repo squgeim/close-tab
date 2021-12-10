024536456a6d4198eb9247cdb15ea274a8b0a626
squgeim <shreyadahal@gmail.com>
2021-12-09 22:36:42 -0500

    fix: do not depend on installed event

--- background.js
+++ background.js
@@ -1,10 +1,8 @@
-chrome.runtime.onInstalled.addListener(() => {
-  chrome.action.onClicked.addListener(async () => {
-    const [ tab ] = await chrome.tabs.query({
-      active: true,
-      currentWindow: true 
-    });
-
-    await chrome.tabs.remove(tab.id);
+chrome.action.onClicked.addListener(async () => {
+  const [ tab ] = await chrome.tabs.query({
+    active: true,
+    currentWindow: true,
   });
+
+  await chrome.tabs.remove(tab.id);
 });
